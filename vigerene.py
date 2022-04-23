ce2eb10ee7065d1e0df104cacb264e8d504a476c
Kevin Nguyen <kdotndot@Kevins-MacBook-Pro.local>
2022-04-22 20:45:09 -0700

    .

--- vigerene.py
+++ vigerene.py
@@ -1,7 +1,35 @@
+
+
+
 lines = ""
-with open('vigerene_easy_encrypted.txt', encoding="utf8") as f:
+with open('vigerene_easy_encrypted.txt') as f:
     lines = f.readlines()
 
 line = lines[0]
 line = line.lower()
-print(lines)
+print(line)
+
+#Finding key length 
+for x in range(2, 3):
+    #Finding letter frequencies
+    sequences = []
+    keylen = x
+    for y in range(keylen):
+        sequences.append([])
+    count = 0
+    #Go through the text and separate by 
+    for y in range(len(line)):
+        letter = line[y]
+        if count >= keylen:
+            count = 0
+        if letter.isalpha():
+            sequences[count].append(letter)    
+            count += 1
+
+    
+
+
+            
+                
+
+            
